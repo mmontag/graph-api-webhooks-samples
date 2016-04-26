b270299bd80e3fb15329c1f8e9404e5633aa90ab
Matt Montag <montag@spotify.com>
2016-04-25 17:41:42 -0700

    Add echo responder

--- heroku/index.js
+++ heroku/index.js
@@ -8,7 +8,10 @@
 
 var bodyParser = require('body-parser');
 var express = require('express');
+var request = require('request');
 var app = express();
+
+var PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN || 'no-access-token-in-env';
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
@@ -36,6 +39,27 @@ app.post('/facebook', function(req, res) {
   console.log(req.body);
   // Process the Facebook updates here
   res.sendStatus(200);
+
+  var userId = req.body.entry[0].messaging.sender.id;
+  var message = req.body.entry[0].messaging.message.text;
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages?access_token=" + PAGE_ACCESS_TOKEN,
+    method: "POST",
+    json: {
+      "recipient":{
+        "id": userId
+      },
+      "message":{
+        "text":"You said '" + message + "'."
+      }
+    }
+  }, function(error, response, body) {
+    console.log('Send message returned HTTP ', response.statusCode);
+    if (response.statusCode != 200) {
+      console.log('> ', body);
+    }
+  });
+
 });
 
 app.post('/instagram', function(req, res) {
